0cb291b9283620b0ed9edddb430662f44ab5bf48
smaccoun <steven.maccoun@vorto.ai>
2022-05-10 13:23:38 -0600

    Stub login form

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { FormEvent } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Routes, Route, Link } from "react-router-dom";
@@ -8,18 +8,56 @@ function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Login />} />
         <Route path="/cms" element={<DigitalEventsPage />} />
       </Routes>
     </div>
   );
 }
 
+const Login: React.FC<{}> = ({ }) => {
+  const [inputEmail, setInputEmail] = React.useState<string>('')
+  const [inputPwd, setInputPwd] = React.useState<string>('')
+
+  return (
+    <div className="w-full h-full flex justify-center items-center border h-screen flex-col">
+      <form
+        className='flex flex-col w-1/2 h-64 border justify-between'
+        onSubmit={(e) => {
+
+          e.preventDefault();
+          const target = e.currentTarget as typeof e.currentTarget & {
+            email: { value: string };
+            password: { value: string };
+          };
+          const email = target.email.value; // typechecks!
+          const password = target.password.value;
+
+          alert(`YOU JUST SUBMITTED: ${email} - ${password}`)
+        }}>
+        <label className='font-bold'>
+          Email:
+          <input className='border ml-4' type="email" name="email" value={inputEmail} onChange={(e) => { e.preventDefault(); setInputEmail(e.currentTarget.value) }} />
+        </label>
+        <label className="font-bold">
+          Password:
+          <input className="border ml-4" type="password" name="password" value={inputPwd} onChange={(e) => { e.preventDefault(); setInputPwd(e.currentTarget.value) }} />
+        </label>
+        <input className='rounded border border-blue-500 px-4 py-4' type="submit" value="Submit" />
+      </form>
+
+      <nav className='mt-20 underline'>
+        <Link style={{ fontSize: 30 }} to="/cms">CMS</Link>
+      </nav>
+    </div>
+  )
+}
+
 const Home = () => {
   return (
     <div style={{ display: 'flex', flexDirection: 'column' }}>
       <h1>Home</h1>
-      <nav>
+      <nav className='underline'>
         <Link style={{ fontSize: 30 }} to="/cms">CMS</Link>
       </nav>
     </div>
--- src/Pages/DigitalEventsPageVM.tsx
+++ src/Pages/DigitalEventsPageVM.tsx
@@ -2,6 +2,7 @@ import { createModel, init, Models, RematchDispatch, RematchRootState } from '@r
 import immerPlugin from '@rematch/immer'
 import createSelectPlugin from '@rematch/select'
 import produce from 'immer'
+import { url } from 'inspector'
 import createCachedSelector from 're-reselect'
 import { Root } from 'react-dom/client'
 import { AJStore, Deal } from '../Models'
@@ -19,17 +20,62 @@ export type PageState = {
   isFetching: boolean
 }
 
+type Section =
+  KnownSection
+// | {_ tag: 'templated'} -- TODO: somday allow for flexible
+
+
+type KnownSectionTypes =
+  { _tag: 'FEATURED_DEALS', deals: Deal[] }
+  | KnownSections.FeaturedStores
+  | { _tag: 'ADDITIONAL_STORES', stores: AJStore[] }
+
+class KnownSection {
+  _tag: 'Known' = 'Known'
+  constructor(public section: KnownSectionTypes) { }
+
+  modifyIfFeaturedStore(f: (fs: KnownSections.FeaturedStores) => KnownSections.FeaturedStores) {
+    if (this.section instanceof KnownSections.FeaturedStores) {
+      const modifiedFeaturedSection = f(this.section)
+      this.section = modifiedFeaturedSection
+    }
+  }
+}
+namespace KnownSections {
+  type Tag = 'FEATURED_STORES' | 'ADDITIONAL_STORES'
+  export class FeaturedStores {
+    _tag: Tag = 'FEATURED_STORES'
+    constructor(public stores: AJStore[]) { }
+
+    withAddedStore(store: AJStore): FeaturedStores {
+      return new FeaturedStores([...this.stores.concat(store)])
+    }
+    withRemovedStore(urlSlug: string): FeaturedStores {
+      const mbIdx = this.stores.findIndex(store => store.url_slug = urlSlug)
+      const withRemovedStore = produce(this.stores, draft => {
+        const index = draft.findIndex(s => s.url_slug === urlSlug)
+        if (index !== -1) draft.splice(index, 1)
+      })
+      return new FeaturedStores(withRemovedStore)
+    }
+  }
+}
+
+
+
 export type CreateDigitalEventFormInput = {
   pageTitle: string
-  banner: BannerContent,
-  featuredStores: AJStore[],
-  featuredDeals: Deal[],
+  banner: BannerContent
+  sections: Section[]
+  featuredStores: AJStore[]
+  featuredDeals: Deal[]
   additionalStores: null | { title: string, stores: AJStore[] }
 }
 
 export const emptyFormState: CreateDigitalEventFormInput = {
   pageTitle: '',
   banner: { title: '', cashBackString: '', backgroundImageUrl: null },
+  sections: [],
   featuredStores: [],
   featuredDeals: [],
   additionalStores: null
@@ -62,17 +108,26 @@ export const editModel = createModel<RootModel>()({
     },
 
     addFeaturedStore(state, payload: AJStore) {
+      const withNewSections: Section[] = state.form.sections.map(s => {
+        s.modifyIfFeaturedStore(fss => fss.withAddedStore(payload))
+
+        return s
+      })
       const withNewForm = produce(state.form, draft => {
         draft.featuredStores.push(payload)
       })
-      return { ...state, form: withNewForm }
+      return { ...state, form: { ...withNewForm, sections: withNewSections } }
     },
     removeFeaturedStore(state, payload: string) {
+      const withRemovedStoreSections = state.form.sections.map(s => {
+        s.modifyIfFeaturedStore(ffs => ffs.withRemovedStore(payload))
+        return s
+      })
       const withRemovedStore = produce(state.form.featuredStores, draft => {
         const index = draft.findIndex(s => s.url_slug === payload)
         if (index !== -1) draft.splice(index, 1)
       })
-      return { ...state, form: { ...state.form, featuredStores: withRemovedStore } }
+      return { ...state, form: { ...state.form, featuredStores: withRemovedStore, sections: withRemovedStoreSections } }
     },
     addFeaturedDeal(state, payload: Deal) {
       const withNewForm = produce(state.form, draft => {
@@ -103,11 +158,6 @@ export const editModel = createModel<RootModel>()({
     }
   },
   effects: (dispatch) => ({
-    // async incrementAsync(payload: number, state) {
-    //   console.log("This is current root state", state);
-    //   await new Promise((resolve) => setTimeout(resolve, 1000));
-    //   dispatch.count.increment(payload);
-    // },
   }),
 });
 export interface RootModel extends Models<RootModel> {
